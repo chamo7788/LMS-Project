d26c979ad89e9ea3950a08b32f17c608dba6a87c
Thisaru Shamal <thisarushamal2001@gmail.com>
2024-06-19 01:32:23 +0530

    add expand button and scrolling fuction to side bar

--- Smart LMS/src/components/home/RightSidebar/RightSidebar.jsx
+++ Smart LMS/src/components/home/RightSidebar/RightSidebar.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import "../../../assets/css/home/RightSidebar.css";
 import RightSidebarItems from "./RightsidebarItems";
 import items from "../../../data/RightSidebarData.json";
 
 export function RightSidebar() {
+  const [pinned, setPinned] = useState(false);
+  const togglePinned = () => {
+    setPinned(!pinned);
+  };
   return (
-    <div className="rightsidebar">
+    <div className={`rightsidebar ${pinned ? "pinned" : ""}`}>
+      <i className="pin-button" onClick={togglePinned}>
+        <i
+          className={
+            pinned ? "bi bi-text-indent-left" : "bi bi-text-indent-right"
+          }
+        ></i>
+      </i>
       {items.map((item, index) => (
         <RightSidebarItems key={index} item={item} />
       ))}
