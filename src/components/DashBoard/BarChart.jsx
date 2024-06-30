c676d407899ed02b34d829e109bc2d6133bccc56
Thisaru Shamal <thisarushamal2001@gmail.com>
2024-06-30 21:10:03 +0530

    added bar chart and pie chart components

---
+++ src/components/DashBoard/BarChart.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../assets/css/DashBoard/barChart.css";
+
+export function BarChart() {
+  return <div className="barchart">BarChart</div>;
+}
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Navbar, LeftSideBar } from "../components";
+import { Navbar, LeftSideBar, BarChart, PieChart } from "../components";
 
 export default function DashBoard() {
   return (
     <div>
       <Navbar />
       <LeftSideBar />
+      <BarChart />
+      <PieChart />
     </div>
   );
 }
