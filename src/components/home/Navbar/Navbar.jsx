71ebd4e8778a21684ccc6e3e36933f268a37ee80
IsiraWeerasinghe8305 <isira.aw@gmail.com>
2024-07-01 23:34:24 +0530

    added a loging image

--- src/components/home/Navbar/Navbar.jsx
+++ src/components/home/Navbar/Navbar.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from "react-router-dom";
 import '../../../assets/css/home/navbar.css';
 import logoLight from '../../../assets/images/logo-black.png';
 import { FaBars, FaTimes, FaEnvelope } from 'react-icons/fa';
+import imagesData from "../../../data/student_Images.json";
 
 export function Navbar() {
-
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
+
+  useEffect(() => {
+    const image = imagesData.images.find((img) => img.id === "1");
+    if (image) {
+      import(`../../../${image.url}`).then(imageModule => {
+        setSelectedImage({
+          ...image,
+          url: imageModule.default
+        });
+      }).catch(err => console.error("Failed to load image:", err));
+    }
+  }, []);
 
   return (
     <nav className='navbar'>
@@ -30,20 +43,15 @@ export function Navbar() {
           <NavLink to="/about" onClick={toggleMobileMenu} className='navbar__menu-link' activeClassName="active">About</NavLink>
         </li>
         <li className='navbar__menu-item'>
-          <NavLink to="/3" onClick={toggleMobileMenu} className='navbar__menu-link' activeClassName="active">Services</NavLink>
+          <NavLink to="/services" onClick={toggleMobileMenu} className='navbar__menu-link' activeClassName="active">Services</NavLink>
         </li>
         <li className='navbar__menu-item'>
-          <NavLink to="/4" onClick={toggleMobileMenu} className='navbar__menu-link' activeClassName="active">Contact</NavLink>
+          <NavLink to="/contact" onClick={toggleMobileMenu} className='navbar__menu-link' activeClassName="active">Contact</NavLink>
         </li>
       </ul>
       <div className='navbar__searchbox'>
         <input type="text" placeholder='Search' />
         <i className="fa-solid fa-magnifying-glass"></i>
-      </div>
-      <div className='navbar__login'>
-        <NavLink to="/login">
-          <button className='navbar__login-button'>Login</button>
-        </NavLink>
       </div>
       <div className='navbar__icons'>
         <div className='navbar__message'>
@@ -52,10 +60,21 @@ export function Navbar() {
           </NavLink>
         </div>
       </div>
-
       <div className='navbar__mobile-menu-icon' onClick={toggleMobileMenu}>
         {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
       </div>
-    </nav>
+
+
+      <div className='navbar__login'>
+        <NavLink to="/login">
+          <button className='navbar__login-button'>Login</button>
+        </NavLink>
+        {selectedImage && (
+          <NavLink className='userDetails_pic'>
+            <img src={selectedImage.url} alt="User" className='Login_pic' />
+          </NavLink>
+        )}
+      </div>
+   </nav>
   );
 }
