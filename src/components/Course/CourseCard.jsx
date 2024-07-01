25074e04b0ed5b251f5dc064c21ced6d35dad8c1
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2024-07-02 02:06:50 +0530

    update curses

--- src/components/Course/CourseCard.jsx
+++ src/components/Course/CourseCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../assets/css/Course/courseCard.css";
+import courses from "../../data/courses.json";
 
-export function CourseCard({ image, title, instructor, subtitle }) {
+export function CourseCard({ courseId, image, title, instructor, subtitle }) {
     return (
-        <div className='card-containter'>
-            <div className="card-image">
-                <img src={image} alt={title} className='card-logo'/>
+        <Link to={`/courses/course/${courseId}`} className="card-link">
+            <div className="card-container">
+                <div className="card-image">
+                    <img src={image} alt={title} className="card-logo" />
+                </div>
+                <div className="card-content">
+                    <p className="card-title">{title}</p>
+                    <p className="card-instructor">{instructor}</p>
+                    <p className="card-subtitle">{subtitle}</p>
+                </div>
             </div>
-            <div className="card-content">
-                <p className="card-title">{title}</p>
-                <p className="card-instructor">{instructor}</p>
-                <p className="card-subtitle">{subtitle}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
