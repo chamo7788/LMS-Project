4745b5e9dadcfb51d66188d49af15d84e9d21f8d
shakya1226 <160268366+shakya1226@users.noreply.github.com>
2024-07-02 13:45:30 +0530

    add footer

--- src/components/home/HomePage/Homepage.jsx
+++ src/components/home/HomePage/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../../../assets/css/home/homepage.css";
-import { Link } from "react-router-dom";
+
 
 export function Homepage(){
   return(
@@ -69,40 +69,7 @@ export function Homepage(){
 
     </section>  
 
-    <footer className='footer'>
-        <div className='footer-container'>
-          <div className='footer-row'>
-            <div className='footer-column'>
-              <h3>About Us</h3>
-              <p>Learn more about our platform and mission.</p>
-            </div>
-            <div className='footer-column'>
-              <h3>Quick Links</h3>
-              <ul>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/messenger">Messenger</Link></li>
-              <li><Link to="/">About</Link></li>
-              <li><Link to="/3">Services</Link></li>
-              <li><Link to="/4">Contact</Link></li>
-              </ul>
-            </div>
-            <div className='footer-column'>
-              <h3>Contact Us</h3>
-              <p>Email: info@lfjhbdje.com</p>
-              <p>Phone: 071 3451234</p>
-            </div>
-            <div className='footer-column'>
-              <h3>Follow Us</h3>
-              <div className='social-icons'>
-                <a href='#'><i className='fab fa-facebook-f'></i></a>
-                <a href='#'><i className='fab fa-twitter'></i></a>
-                <a href='#'><i className='fab fa-linkedin-in'></i></a>
-                <a href='#'><i className='fab fa-instagram'></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
+    
   
    </>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {Navbar} from "../components";
 import { Homepage } from "../components/home/HomePage/Homepage";
+import {Footer} from "../components/home/Footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Homepage/>
+      <Footer/>
     </div>
   );
 }
