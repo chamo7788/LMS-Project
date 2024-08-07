72eb69baa8ecb827c9bba31ca7bc9f740424355e
shakya1226 <160268366+shakya1226@users.noreply.github.com>
2024-08-07 21:52:30 +0530

    add course video

--- src/components/Course/CourseVideo.jsx
+++ src/components/Course/CourseVideo.jsx
@@ -1,9 +1,48 @@
 import React from "react";
+import ReactPlayer from 'react-player';
+import { Link } from 'react-router-dom';
+import "../../assets/css/Course/courseVideo.css";
+import video from "../../assets/videos/v1.mp4";
 
 export function CourseVideo() {
+  
   return (
-    <div>
-      <h1>Course Video</h1>
+    <div className="video_page">
+      <div className="path">
+        <Link to="/home">
+          <span>Home</span>
+        </Link>
+        <span>&gt;</span>
+        <Link to="/home">
+          <span>Module 1</span>
+        </Link>
+        <span>&gt;</span>
+        <Link to="/home">
+          <span>Looping constructs</span>
+        </Link>
+      </div>
+      <div className="video">
+      <video controls controlsList="nodownload" width="900" height="600">
+          <source src={video} type="video/mp4" />
+         {/* <track 
+            src={subtitles} 
+            kind="subtitles" 
+            srcLang="en" 
+            label="English"
+            default 
+          />*/}
+         
+       </video>
+        {/*<ReactPlayer 
+          url={"https://www.youtube.com/watch?v=6gcaueR_TPg"} 
+          light={true} 
+          controls={true} 
+          height="400px" 
+          width="700px" 
+        />*/}
+       
+      </div> 
     </div>
+    
   );
 }
