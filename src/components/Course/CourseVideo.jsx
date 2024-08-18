3cb38c2ea3550f7b47915c6534016cee46370373
shakya1226 <160268366+shakya1226@users.noreply.github.com>
2024-08-18 11:56:49 +0530

    adjust video and path

--- src/components/Course/CourseVideo.jsx
+++ src/components/Course/CourseVideo.jsx
@@ -7,42 +7,42 @@ import video from "../../assets/videos/v1.mp4";
 export function CourseVideo() {
 
   return (
+  <><section className="path">
+      <Link to="/home">
+        <span>Home</span>
+      </Link>
+      <span>&gt;</span>
+      <Link to="/home">
+        <span>Module 1</span>
+      </Link>
+      <span>&gt;</span>
+      <Link to="/home">
+        <span>Looping constructs</span>
+      </Link>
+    </section>
     <div className="video_page">
-      <div className="path">
-        <Link to="/home">
-          <span>Home</span>
-        </Link>
-        <span>&gt;</span>
-        <Link to="/home">
-          <span>Module 1</span>
-        </Link>
-        <span>&gt;</span>
-        <Link to="/home">
-          <span>Looping constructs</span>
-        </Link>
-      </div>
       <div className="video">
         <video controls controlsList="nodownload" width="900" height="600">
-          <source src={video} type="video/mp4" />
-          {/* <track 
-            src={subtitles} 
-            kind="subtitles" 
-            srcLang="en" 
-            label="English"
-            default 
-          />*/}
+         <source src={video} type="video/mp4" />
+            {/* <track
+      src={subtitles}
+      kind="subtitles"
+      srcLang="en"
+      label="English"
+      default
+    />*/}
 
-        </video>
-        {/*<ReactPlayer 
-          url={"https://www.youtube.com/watch?v=6gcaueR_TPg"} 
-          light={true} 
-          controls={true} 
-          height="400px" 
-          width="700px" 
-        />*/}
+          </video>
+          {/*<ReactPlayer
+      url={"https://www.youtube.com/watch?v=6gcaueR_TPg"}
+      light={true}
+      controls={true}
+      height="400px"
+      width="700px"
+    />*/}
 
-      </div>
-    </div>
+        </div>
+      </div></>
 
   );
 }
