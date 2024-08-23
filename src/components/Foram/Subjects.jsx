eabe7fa7d0d9f4c56780b556d7a2a09ad2842210
Chamodya Ganegamage <139947960+chamo7788@users.noreply.github.com>
2024-08-23 21:36:15 +0530

    update some files

--- src/components/Foram/Subjects.jsx
+++ src/components/Foram/Subjects.jsx
@@ -2,29 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import '../../assets/css/Foram/subjects.css';
 import subjects from "../../data/subjects.json";
+import { ScrollToTopButton } from './scroll-to-top';
 
 export function Subjects({ onCreateQuestionClick }) {
     return (
         <div className="findAnswer-container">
-            <div className="subjects">
-                {subjects.map((subject, index) => (
-                    <div className="subject-card" key={index}>
-                        <Link to={subject.link}>
-                            <p className="subjects-title">{subject.Subjecttitle}</p>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-            <div className="findAnswer-nevbar">
+            <div className="findAnswer-navbar">
+                <select className="dropdown-subjects">
+                    {subjects.map((subject, index) => (
+                        <option className="options" key={index}>
+                            <Link to={subject.link}>{subject.Subjecttitle}</Link>
+                        </option>
+                    ))}
+                </select>
                 <div className="dropdown-sort">
                     <select>
-                        <option value="your">Your</option>
-                        <option value="new">New</option>
-                        <option value="top">Top</option>
-                        <option value="best">Best</option>
+                        <option className="options" value="your">Your</option>
+                        <option className="options" value="new">New</option>
+                        <option className="options" value="top">Top</option>
+                        <option className="options" value="best">Best</option>
                     </select>
                 </div>
-                <div className="creact-question" onClick={onCreateQuestionClick}>
+                <div className="create-question" onClick={onCreateQuestionClick}>
                     <span className="create-title">Create</span>
                     <i className="fa-solid fa-plus"></i>
                 </div>
@@ -32,6 +31,7 @@ export function Subjects({ onCreateQuestionClick }) {
                     <i className="fa-regular fa-bell"></i>
                 </div>
             </div>
+            <ScrollToTopButton />
         </div>
     );
 }
