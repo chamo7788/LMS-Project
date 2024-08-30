de3c439e220720fdb30ea40255df98c6c8b10340
Thisaru Shamal <thisarushamal2001@gmail.com>
2024-08-30 14:52:05 +0530

    edited dashboard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -8,21 +8,29 @@ import {
   Profilecards,
   Footer,
   DBCourses,
+  EditProfile
 } from "../components";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 export default function DashBoard() {
+  const location = useLocation();
+  const isCoursePreview = location.pathname.includes('/EditProfile');
+
   return (
     <div>
       <Navbar />
-      <LeftSideBar />
-      <DBCourses />
+      <Profilecards />
+      <LeftSideBar />     
       <div className="charts">
         <TimeChart />
         <PieChart />
       </div>
-      <Profilecards />
+      <DBCourses />
       <TodoList />
       <Footer />
+      <Routes>
+        <Route path="/dashBoard/editProfile" element={<EditProfile />} />
+      </Routes>
     </div>
   );
 }
